79a7ea5708448f0eae41530a14e74a653efb356d
tymshey <tymon.s@interia.eu>
2026-07-31 15:03:05 +0200

    przyspieszenie er
    przyspieszono ery age of absolutism i revolutions
    zmniejszono czas przekraczania terry incognity
    uprzątnięto niektóre niepotrzebne rzeczy

--- common/defines/t_defines.lua
+++ common/defines/t_defines.lua
@@ -1,2 +1,3 @@
 NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.05			-- changed from 0.02
 NDefines.NEconomy.NAVAL_TECH_MAINTENANCE_IMPACT = 0.06			-- changed from 0.02
+NDefines.NMilitary.EXPLORATION_TRAVEL_TIME = 2					-- Multiplier for travel time when exploring
