4d40e603c98788d48e6b64f593e4c48733ab3602
tymshey <tymon.s@interia.eu>
2026-09-02 11:13:05 +0200

    supply depot

--- common/defines/t_defines.lua
+++ common/defines/t_defines.lua
@@ -1,3 +1,5 @@
 NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.05			-- changed from 0.02
 NDefines.NEconomy.NAVAL_TECH_MAINTENANCE_IMPACT = 0.06			-- changed from 0.02
 NDefines.NMilitary.EXPLORATION_TRAVEL_TIME = 2					-- Multiplier for travel time when exploring
+NDefines.NMilitary.SUPPLY_DEPOT_DURATION_MONTHS = 60			-- Time until supply depot is removed.
+NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 5					-- Mil power cost to build a supply depot in an area.
